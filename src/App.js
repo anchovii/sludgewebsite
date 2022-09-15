156c26ec93bd0c2e87d3b8660e6fb3099dc2a50b
anchovii <108648969+anchovii@users.noreply.github.com>
2022-09-14 20:43:59 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" >
+      <header className="App-header">
+        <img src={logo} className="App-logo" class="sepia w-3/12 sm:w-4/12"alt="logo"/>
+        <p>
+          ack okay
+        </p>
+        <h1 className="text-3xl font-bold">
+      <div class="p-4 shadow-sm bg-purple-500 h-24 rounded text-sky-300">[][aa][]</div>
+      <div class="p-4 shadow-sm border-indigo-600 h-16 w-auto font-medium">
+        wait how do i <a class='underline text-indigo-200'href="https://candybox2.github.io/">link</a> things 
+        again??</div>
+    </h1>
+
+      <p class="px-10">i love going onto <a class='underline text-indigo-200' href="https://kittensgame.com/web/#">kittensgame</a> and
+      taking a single look at the amount of time needed to get enough parchment for the next
+       research and immediately closing the window</p>
+        <br></br>
+       <p>ughhh we invented css for a reason how do i <a>add classnames and stuff</a> to this</p>
+       <h2>like howww do i <a>add funny colours to text</a> without having to duplicate the same settings over and over</h2>
+      </header>
+      <div class="bg-slate-200, dark:bg-slate-600, text-purple-500">
+      <p>
+      suppose i'll find out later.
+      anyways,,,
+
+      </p>
+      </div>
+      <div class="bg-slate-700 text-indigo-100 font-sans text-3xl md:text-4xl p-7 flex flex-row-reve border-solid rounded-b-xl border-slate-600">
+
+      <div class="duration-200 hover:bg-slate-500 px-1.5 border-r-2"><a href="https://candybox2.github.io">Home</a></div>
+      <div class="duration-200 hover:bg-slate-500 px-1.5 border-r-2">Hi</div>
+      <div class="duration-200 hover:bg-slate-500 px-1.5 border-r-2">What</div>
+
+      </div>
+      <div class="p-5 font-sans text-medium text-slate-900 ">
+        <p class="rounded border-y-4 bg-slate-200 p-8 md:text-lg sm:text-base md:mx-20 border-slate-600 border-solid justify-center">
+          <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.vV76lg24toj_9EjRfXYwDgHaCH%26pid%3DApi&f=1">
+            </img>&amp;BTW this is my website. aa ee ii oo uu. OKay how does text work</p>
+        </div><div class="p-5 font-sans text-medium sm:text-xm text-slate-900 flex flex-wrap">
+        <div class="rounded border-y-4 border-slate-600 bg-slate-200 p-4 flex-auto w-32 m-2 italic">
+          "I love Chromaticrafts research system" -someone</div>
+        <div class="rounded border-y-4 border-slate-600 bg-slate-200 p-4 flex-auto w-64 m-2"><img title="Slop"class=" justify-center border-4 duration-100 hover:border-slate-300" alt="minecraft slime ball" 
+        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.EzUvTiX1hQeu2UEnmtg3MAAAAA%26pid%3DApi&f=1"></img></div>
+        <div class="rounded border-y-4 border-slate-600 bg-slate-200 p-4 flex-auto w-32 m-2">Hi</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
